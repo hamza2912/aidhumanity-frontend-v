37cf7732a6ae205622d5e3f6063744ebf0df3519
muhammadfuaad <fuaad29@gmail.com>
2023-03-30 12:34:47 +0500

    code

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 export const currencyFormatter = number =>
-  new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' , currencyDisplay: "symbol" }).format(
+  new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' , currencyDisplay: "symbol", minimumIntegerDigits: 4 , minimumFractionDigits: 0,
+  maximumFractionDigits: 0, useGrouping: true }).format(
     number
   );
