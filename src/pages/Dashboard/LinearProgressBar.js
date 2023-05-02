2fe62aa8c20a288b698b8e41d09521d7b8795e8f
muhammadfuaad <fuaad29@gmail.com>
2023-05-02 12:41:15 +0500

    removed unnecessary code and corrected naming convention

--- src/pages/Dashboard/LinearProgressBar.js
+++ src/pages/Dashboard/LinearProgressBar.js
@@ -2,20 +2,14 @@ import React from 'react'
 
 function LinearProgressBar(props) {
 	
-	const parentDiv = {
+	const fullDiv = {
 		height: 10,
     width: "100%",
 		backgroundColor: '#f1f1f1',
 		borderRadius: 40,
 	}
-  const parentDiv2 = {
-		height: 10,
-    width: "100%",
-		backgroundColor: '#f1f1f1',
-		borderRadius: 40,
-	}
-	
-	const childDiv = {
+  
+	const filledDiv = {
 		height: '100%',
 		width: `${props.progress}%`,
 		backgroundColor: "#00ade9",
@@ -33,14 +27,14 @@ function LinearProgressBar(props) {
     <>
       {props.isTextRight ? (
         <div className='flex gap-8 items-center'>
-          <div style={parentDiv}>
-            <div style={childDiv}></div>
+          <div style={fullDiv}>
+            <div style={filledDiv}></div>
           </div>
           <span style={progressText}>{`${props.progress}%`}</span>
         </div>
       ) : props.isTextBelow ? (
-        <div className='relative' style={parentDiv2}>
-          <div style={childDiv}></div>
+        <div className='relative' style={fullDiv}>
+          <div style={filledDiv}></div>
           <span className='absolute right-0 top-5' style={progressText}>{`${props.progress}%`}</span>
         </div>
       ) : null}
