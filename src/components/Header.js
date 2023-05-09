b1b333f60b87fad3baf2ea6cae30b087c0779fab
muhammadfuaad <fuaad29@gmail.com>
2023-05-09 10:57:07 +0500

    fixed the placement of log out button in main header

--- src/components/Header.js
+++ src/components/Header.js
@@ -200,12 +200,11 @@ function Header({ showDonateButton = false }) {
                   Log Out
                 </button>}
               </div>
-<<<<<<< HEAD:src/components/Header.js
             </div>   
-=======
-            </div>
-            
->>>>>>> fd51f82 (added log out button to header):src/components/header.js
+
+          
+           
+
           </nav>
           
           {showAppealModal ? (
