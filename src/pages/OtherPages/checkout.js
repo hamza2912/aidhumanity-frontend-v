a702b7007c54531051c7a3d966896a5e901dba10
muhammadfuaad <fuaad29@gmail.com>
2023-05-09 18:11:28 +0500

    capitalized header.js

--- src/pages/OtherPages/checkout.js
+++ src/pages/OtherPages/checkout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../../components/Footer';
-import Header from '../../components/header';
+import Header from '../../components/Header';
 import Switch from '../../components/switch/switch';
 
 function Checkout() {
