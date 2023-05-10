31fddacf6466e3880bfe96567eb1826a1e94d919
muhammadfuaad <fuaad29@gmail.com>
2023-05-10 12:16:10 +0500

    text styling fixed

--- src/components/HistoryRow.js
+++ src/components/HistoryRow.js
@@ -13,16 +13,16 @@ const HistoryRow = ({
 }) => {
   return (
     <tr onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} key={key}>
-      <td className="flex gap-2 items-center text-blue-dark text-sm font-semibold">
+      <td className="flex gap-2 items-center text-blue-dark text-[0.69rem] font-semibold">
         <img src="/Icons/icon_user_circle_blue.svg"></img>
         {name}
       </td>
-      <td className="text-sm text-black-50">{country}</td>
-      <td className="text-sm text-black-50 font-medium flex items-center gap-2">
-        <img src="/Icons/icon_calendar_clock.svg"></img>
+      <td className="text-[0.69rem] text-black-50">{country}</td>
+      <td className="text-[0.69rem] text-black-50 font-medium flex items-center gap-2">
+        <img src="/Icons/icon_calendar_clock.svg" className="w-6"></img>
         {date}
       </td>
-      <td className="text-blue text-sm font-semibold">{amount}</td>
+      <td className="text-blue text-[0.69rem] font-semibold">{amount}</td>
       <td
         className="cursor-pointer"
         onClick={() => {
