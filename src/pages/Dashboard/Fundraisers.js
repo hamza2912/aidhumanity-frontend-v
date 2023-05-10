9561734f5ddbdda9fd4b0ca6471ca573d2ddd8ea
muhammadfuaad <fuaad29@gmail.com>
2023-05-10 10:54:00 +0500

    separator line and profile icon added to fundraisers section

--- src/pages/Dashboard/Fundraisers.js
+++ src/pages/Dashboard/Fundraisers.js
@@ -33,13 +33,15 @@ export const Fundraisers = ({ campaigns }) => {
               />
             </div>
             <div className="flex justify-between items-center mt-2">
-              <div className="flex gap-2 w-auto">
+              <div className="flex gap-2 items-center w-auto">
                 <p className="text-xs lg:text-sm font-semibold text-gray-300">
                   <span className="text-blue">
                     £{campaign.raised_amount / 100 || 0} /{' '}
                   </span>
                   {campaign.targeted_amount / 100 || 0}
                 </p>
+                <div class="h-3 w-1 border-l-2 border-dgray"></div>
+                <img src="/Icons/icon_user_circle.svg"></img>
                 <p className="text-gray-600 lg:text-sm text-xs font-medium flex gap-1 items-center">
                   {campaign.supporters_count || 0} supporters
                 </p>
