9c93a3a495c0847aa3d557720e5f11ebafae873d
muhammadfuaad <fuaad29@gmail.com>
2023-05-15 18:23:59 +0500

    cleaned PR

--- src/pages/OtherPages/contact.js
+++ src/pages/OtherPages/contact.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { ContactForm } from '../../components/ContactForm';
-import Switch from '../../components/switch/switch';
 
 function Contact() {
   return (
