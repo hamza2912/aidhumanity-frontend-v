9e43ed902fc8e74e92fb17c9940d015d7eb6d8ac
muhammadfuaad <fuaad29@gmail.com>
2023-05-24 17:59:51 +0500

    contact form's input field styling fixed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -33,52 +33,50 @@ export const ContactForm = () => {
         Contact Form
       </h1>
       <form className="w-full h-auto mt-6" action="">
-        <div
-          className="w-full h-auto border border-lgray rounded-lg flex flex-col p-2 mt-4"
-          action=""
-        >
-          <label
-            className="text-mont text-dgray text-xs font-semibold"
-            htmlFor="fullName"
-          >
-            Full Name
-          </label>
+
+        <div className='relative mt-4'>
           <input
-            className="text-mont text-sm text-black-50 font-semibold focus:outline-none"
-            type="text"
             name="fullName"
             value={formData.fullName}
             onChange={handleChange}
+            className="w-full pt-5 pb-1 px-3 rounded-md text-black font-medium text-[13px] border border-gray-200 border-lblue focus:outline-none z-10"
+            type="text"
           />
-        </div>
-        <div
-          className="w-full h-auto border border-lgray rounded-lg flex flex-col p-2 mt-4"
-          action=""
-        >
           <label
-            className="text-mont text-dgray text-xs font-semibold"
-            htmlFor="email"
+            className="text-dgray font-semibold absolute top-2 left-3 text-[11px]"
+            for="first_name"
           >
-            Email
-          </label>
+            Full Name *
+          </label>     
+        </div>
+
+        <div className='relative mt-4'>
           <input
-            className="text-mont text-sm text-black-50 font-semibold focus:outline-none"
+            className="w-full pt-5 pb-1 px-3 rounded-md text-black font-medium text-[13px] border border-gray-200 border-lblue focus:outline-none z-10"
             type="text"
             name="email"
             value={formData.email}
             onChange={handleChange}
           />
+          <label
+            className="text-dgray font-semibold absolute top-2 left-3 text-[11px]"
+            for="first_name"
+          >
+            Email *
+          </label>     
         </div>
+
+
         <input
-          className="w-full h-auto focus:outline-none border rounded-lg border-lgray p-2 mt-4"
+          className="font-medium text-[13px] w-full h-auto focus:outline-none border-lblue rounded-lg border border-gray-200 px-2 py-3 mt-4"
           type="text"
           name="phone"
           value={formData.phone}
           onChange={handleChange}
-          placeholder="Phone *"
+          placeholder="Phone"
         />
         <input
-          className="w-full h-auto focus:outline-none border rounded-lg border-lgray p-2 mt-4"
+          className="font-medium text-[13px] w-full h-auto focus:outline-none rounded-lg border border-gray-200 px-2 py-3 mt-4"
           type="text"
           name="subject"
           value={formData.subject}
@@ -86,7 +84,7 @@ export const ContactForm = () => {
           placeholder="Subject *"
         />
         <textarea
-          className="w-full h-auto focus:outline-none border rounded-lg border-lgray p-2 mt-4"
+          className="font-medium text-[13px] w-full h-auto focus:outline-none rounded-lg border border-gray-200 px-2 py-3 mt-4"
           name="message"
           value={formData.message}
           onChange={handleChange}
