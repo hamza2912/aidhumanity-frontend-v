1d80463a726d016435997913ee059cafe27118f6
muhammadfuaad <fuaad29@gmail.com>
2023-06-03 16:44:23 +0500

    percentage styling fixed

--- src/pages/AppealDetails/CircularProgressBar.js
+++ src/pages/AppealDetails/CircularProgressBar.js
@@ -66,14 +66,17 @@ function CircularProgressBar(props) {
           // top: '50%',
           // left: 0,
           width: '100%',
-          transform: `translateY(105%) ${percentage > 9 && percentage < 100 ? 'translateX(28%)' : percentage < 10 ? 'translateX(35%)' : percentage > 99 ? 'translateX(20%)' : ''}`,
+          transform: `translateY(105%) ${percentage > 9 && percentage < 100 ? 'translateX(32%)' : percentage < 10 ? 'translateX(40%)' : percentage > 99 ? 'translateX(25%)' : ''}`,
           fontWeight: 'bold',
           color: '#fff',
           zIndex: '1',
           fontSize: fontSize,
         }}
       >
-        {percentage}%
+        <div className='flex'>
+          <span>{percentage}</span>
+          <span className='text-[0.5rem] mt-1 font-normal'>%</span>
+        </div>
       </div>
     </div>
   );
