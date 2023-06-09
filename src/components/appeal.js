e01f62b88661723927179dcd8c2815b9f0697b99
muhammadfuaad <fuaad29@gmail.com>
2023-06-09 20:07:40 +0500

    removed extra code

--- src/components/appeal.js
+++ src/components/appeal.js
@@ -14,6 +14,7 @@ function Appeal() {
   const [isHovering, setIsHovering] = useState(false);
   const navigate = useNavigate();
   const { user, dashboardInfo } = useSelector(state => state.session);
+  
   const dispatch = useDispatch();
 
   const handleMouseEnter = () => {
@@ -34,7 +35,7 @@ function Appeal() {
 
   return (
     <>
-      <div id="appeal" className="w-appeal h-fit bg-white shadow-lg border-l-2 relative overflow-x-hidden">
+      <div className="w-appeal h-fit bg-white shadow-lg border-l-2 relative overflow-x-hidden">
         <div className="flex items-center justify-between py-5 border-b-2 px-6 h-20">
           <button className="py-2 px-3 bg-blue-dark hover:bg-sblue text-white font-semibold text-sm rounded-lg hidden lg:block">
             + NEW APPEAL
