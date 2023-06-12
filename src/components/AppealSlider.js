ab17ada726cd4d109f2f2209f84f66f0c824339c
zeshan <zeshanahmedoff@gmail.com>
2023-06-12 15:11:37 +0500

    home page improvement

--- src/components/AppealSlider.js
+++ src/components/AppealSlider.js
@@ -3,7 +3,7 @@ import { SERVER_URL } from '../services/config';
 import { textTruncate } from '../constants';
 import { currencyFormatter } from '../utils';
 import { AppealTags } from '../constants';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DonateModal from './modal/DonateModal';
 import CircularProgressBar from '../pages/AppealDetails/CircularProgressBar';
 import { convertToTitleCase } from '../constants/index';
@@ -65,130 +65,131 @@ function AppealSlider({ appeals = [] }) {
   }, []);
 
   return (
-    <div className="owl-carousel owl-theme appeal-section-carousel w-full h-auto flex items-center justify-around bg-transparent z-10 gap-4">
-      {appeals.map((appeal, index) => {
-        const {
-          targeted_amount,
-          raised_amount,
-          category,
-          title,
-          description,
-          story,
-          offline_donations,
-          end_at,
-          appeal_tag,
-          cover_image,
-          donations_count,
-          id,
-        } = appeal;
+    <>
+      <div className="owl-carousel owl-theme appeal-section-carousel w-full h-auto flex items-center justify-around bg-transparent z-10 gap-4">
+        {appeals.map((appeal, index) => {
+          const {
+            targeted_amount,
+            raised_amount,
+            category,
+            title,
+            description,
+            story,
+            offline_donations,
+            end_at,
+            appeal_tag,
+            cover_image,
+            donations_count,
+            id,
+          } = appeal;
 
-        return (
-          <div class="item h-auto rounded-b-2xl rounded-t-xl py-2 shadow-lg">
-            <div className="relative">
-              <img
-                className="rounded-t-xl max-h-230 w-100 appeal-card"
-                src={SERVER_URL + cover_image}
-                alt="carousel_image_1"
-              />
-              <div className="w-auto bg-black absolute right-5 top-5 px-4 py-2 rounded-xl bg-opacity-60">
-                <p className="text-gray-400 font-medium"> {category?.name}</p>
+          return (
+            <div class="item h-auto rounded-b-2xl rounded-t-xl py-2 shadow-lg">
+              <div className="relative">
+                <img
+                  className="rounded-t-xl max-h-230 w-100 appeal-card"
+                  src={SERVER_URL + cover_image}
+                  alt="carousel_image_1"
+                />
+                <div className="w-auto bg-black absolute right-5 top-5 px-4 py-2 rounded-xl bg-opacity-60">
+                  <p className="text-gray-400 font-medium"> {category?.name}</p>
+                </div>
               </div>
-            </div>
-            <div class="pl-10 pr-6 pt-8 pb-6">
-              <div class="lg:h-36 h-auto">
-                <h2 class="text-xl font-bold text-mont text-black-50">
-                  {title}
-                </h2>
-                <p class="lg:text-base text-sm text-mont text-gray-600 mt-2">
-                  {textTruncate(description, 80)}
-                </p>
-              </div>
-              {donations_count > 0 ? (
-                <div className="flex flex-row items-center mt-4 h-12">
-                  <div className="w-1/5 mr-2">
-                    <CircularProgressBar
-                      percentage={Math.round(
-                        (raised_amount / targeted_amount) * 100
-                      )}
-                      style={{
-                        width: '4rem',
-                        height: '4rem',
-                        fontSize: '0.9rem',
-                      }}
-                    />
-                  </div>
-                  <div className="w-full flex justify-between mt-2">
-                    <div className="flex flex-col">
-                      <span className="text-[11px] text-mont text-blue font-bold">
-                        Raised: {currencyFormatter(raised_amount)}
-                      </span>
-                      <span className="text-[11px] text-mont text-lblack font-medium flex gap-1">
-                        by{' '}
-                        <img
-                          src="/Icons/icon_user_circle_gray.svg"
-                          className="w-4"
-                        ></img>{' '}
-                        <span className="font-semibold">
-                          {donations_count} supporters
-                        </span>
-                      </span>
-                    </div>
-                    <div className="flex flex-col gap-1 items-end">
-                      <span className="text-[11px] text-mont text-green font-semibold">
-                        Goal: {currencyFormatter(targeted_amount)}
-                      </span>
-                      <div className="w-5">
-                        <div
-                          className="bg-yellow flex justify-center items-center rounded-full h-6 w-6 font-semibold text-xs"
-                          onMouseEnter={() => handleMouseEnter(index)}
-                          onMouseLeave={() => handleMouseLeave(index)}
-                        >
-                          <span className="cursor-default">
-                            {getDonationTag(appeal_tag)}
-                          </span>
-                        </div>
-                        {showBadgeArr[index] && (
-                          <div className="bg-white rounded-xl pl-8 pr-5 py-4 shadow-lg absolute -top-20 -right-16">
-                            <p className="text-sm text-gray-600">
-                              This appeal is {convertToTitleCase(appeal_tag)}{' '}
-                              applicable.
-                            </p>
-                          </div>
+              <div class="pl-10 pr-6 pt-8 pb-6">
+                <div class="lg:h-36 h-auto">
+                  <h2 class="text-xl font-bold text-mont text-black-50">
+                    {title}
+                  </h2>
+                  <p class="lg:text-base text-sm text-mont text-gray-600 mt-2">
+                    {textTruncate(description, 80)}
+                  </p>
+                </div>
+                {donations_count > 0 ? (
+                  <div className="flex flex-row items-center mt-4 h-12">
+                    <div className="w-1/5 mr-2">
+                      <CircularProgressBar
+                        percentage={Math.round(
+                          (raised_amount / targeted_amount) * 100
                         )}
+                        style={{
+                          width: '4rem',
+                          height: '4rem',
+                          fontSize: '0.9rem',
+                        }}
+                      />
+                    </div>
+                    <div className="w-full flex justify-between mt-2">
+                      <div className="flex flex-col">
+                        <span className="text-[11px] text-mont text-blue font-bold">
+                          Raised: {currencyFormatter(raised_amount)}
+                        </span>
+                        <span className="text-[11px] text-mont text-lblack font-medium flex gap-1">
+                          by{' '}
+                          <img
+                            src="/Icons/icon_user_circle_gray.svg"
+                            className="w-4"
+                          ></img>{' '}
+                          <span className="font-semibold">
+                            {donations_count} supporters
+                          </span>
+                        </span>
+                      </div>
+                      <div className="flex flex-col gap-1 items-end">
+                        <span className="text-[11px] text-mont text-green font-semibold">
+                          Goal: {currencyFormatter(targeted_amount)}
+                        </span>
+                        <div className="w-5">
+                          <div
+                            className="bg-yellow flex justify-center items-center rounded-full h-6 w-6 font-semibold text-xs"
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={() => handleMouseLeave(index)}
+                          >
+                            <span className="cursor-default">
+                              {getDonationTag(appeal_tag)}
+                            </span>
+                          </div>
+                          {showBadgeArr[index] && (
+                            <div className="bg-white rounded-xl pl-8 pr-5 py-4 shadow-lg absolute -top-20 -right-16">
+                              <p className="text-sm text-gray-600">
+                                This appeal is {convertToTitleCase(appeal_tag)}{' '}
+                                applicable.
+                              </p>
+                            </div>
+                          )}
+                        </div>
                       </div>
                     </div>
                   </div>
+                ) : (
+                  <div className="text-center text-xs text-white hover:bg-dgray p-4 bg-gray-mate rounded-2xl mt-4 h-12">
+                    <p className="cursor-default">
+                      No donation yet, be the first!
+                    </p>
+                  </div>
+                )}
+                <div class="flex justify-between items-center mt-10 pt-4 border-t border-gray-200">
+                  <a
+                    class="text-mont text-nblue font-bold text-xs cursor-pointer"
+                    href=""
+                    onClick={() => handleReadMore(id)}
+                  >
+                    Read More
+                  </a>
+                  <button
+                    class="text-xs font-bold text-white bg-blue hover:bg-nblue rounded-lg px-4 py-3 cursor-pointer"
+                    onClick={() => {
+                      setSelectedAppealId(appeal.id);
+                      setshowDonateModal(true);
+                    }}
+                  >
+                    DONATE NOW
+                  </button>
                 </div>
-              ) : (
-                <div className="text-center text-xs text-white hover:bg-dgray p-4 bg-gray-mate rounded-2xl mt-4 h-12">
-                  <p className="cursor-default">
-                    No donation yet, be the first!
-                  </p>
-                </div>
-              )}
-              <div class="flex justify-between items-center mt-10 pt-4 border-t border-gray-200">
-                <a
-                  class="text-mont text-nblue font-bold text-xs cursor-pointer"
-                  href=""
-                  onClick={() => handleReadMore(id)}
-                >
-                  Read More
-                </a>
-                <button
-                  class="text-xs font-bold text-white bg-blue hover:bg-nblue rounded-lg px-4 py-3 cursor-pointer"
-                  onClick={() => {
-                    setSelectedAppealId(appeal.id);
-                    setshowDonateModal(true);
-                  }}
-                >
-                  DONATE NOW
-                </button>
               </div>
             </div>
-          </div>
-        );
-      })}
-      {/* <div class="item h-auto rounded-b-2xl rounded-t-xl py-2 shadow-lg">
+          );
+        })}
+        {/* <div class="item h-auto rounded-b-2xl rounded-t-xl py-2 shadow-lg">
         <div className="relative">
           <img
             className="rounded-t-xl"
@@ -258,15 +259,24 @@ function AppealSlider({ appeals = [] }) {
           </div>
         </div>
       </div> */}
-      {showDonateModal && (
-        <DonateModal
-          showModal={showDonateModal}
-          setshowModal={setshowDonateModal}
-          quick={false}
-          appealId={selectedAppealId}
-        />
-      )}
-    </div>
+        {showDonateModal && (
+          <DonateModal
+            showModal={showDonateModal}
+            setshowModal={setshowDonateModal}
+            quick={false}
+            appealId={selectedAppealId}
+          />
+        )}
+      </div>
+      <div className="flex lg:justify-end justify-center container mx-auto">
+        <Link
+          to="/appeals"
+          class="transition-colors duration-300 ease-in-out text-center text-nblue text-mont font-medium text-sm border-2 border-lgray rounded-lg px-4 py-2 z-50 hover:bg-blue"
+        >
+          View All
+        </Link>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/home/HomeContact.js
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import HomeService from '../../services/home';
+import { toast } from 'react-toastify';
+
+const HomeContact = () => {
+  const [register, setregister] = React.useState(true);
+  const [showbadge, setshowbadge] = React.useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    // Call the contactUs method
+    const payload = {
+      purpose: register ? 'register' : 'fundraiser',
+      name,
+      email,
+      message,
+    };
+    try {
+      const response = await HomeService.contactUs(payload);
+      setEmail('');
+      setName('');
+      setMessage('');
+      setregister(true);
+      toast.success('We will contact you soon!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <section class="w-full h-auto bg-yellow">
+      <div className="container mx-auto">
+        <div class="lg:w-3/5 w-11/12 mx-auto h-auto py-16">
+          <div class="w-full h-auto">
+            <h1 class="text-3xl text-mont text-black-50 font-bold text-center lg:text-left">
+              Contact Us
+            </h1>
+          </div>
+          <div class="w-full h-auto mt-8 flex lg:flex-row flex-col">
+            <div class="lg:w-1/3 w-full h-auto">
+              <div
+                className={
+                  !register
+                    ? 'w-full h-auto border-4 rounded-xl border-l2black p-4'
+                    : 'w-full h-auto border-4 rounded-xl bg-white border-lblack p-4'
+                }
+              >
+                <div class="w-full h-auto flex justify-between items-center">
+                  <h3
+                    class="text-lg text-black-50 text-mont font-bold flex items-center
+                              "
+                  >
+                    <input
+                      className="mr-2"
+                      type="radio"
+                      id="html"
+                      name="fav_language"
+                      value="HTML"
+                      checked={register}
+                      onClick={() => setregister(true)}
+                    />{' '}
+                    Register
+                  </h3>
+                  <img src="./Icons/user plus.svg" alt="user_plus" />
+                </div>
+                <p class="text-gray-600 text-mont text-mont text-base mt-4">
+                  I want to help Aid Humanity and receive future appeals
+                </p>
+              </div>
+              <div
+                className={
+                  register
+                    ? 'w-full h-auto border-4 rounded-xl border-l2black mt-4 p-4'
+                    : 'w-full h-auto border-4 rounded-xl bg-white border-lblack p-4 mt-4'
+                }
+              >
+                <div class="w-full h-auto flex justify-between items-center">
+                  <h3
+                    class="text-lg text-black-50 text-mont flex items-center font-bold flex items-center
+                              "
+                  >
+                    <input
+                      className="mr-2"
+                      type="radio"
+                      id="html"
+                      name="fav_language"
+                      value="HTML"
+                      checked={!register}
+                      onClick={() => setregister(false)}
+                    />{' '}
+                    Fundraiser
+                  </h3>
+                  <img src="./Icons/Group 15461.svg" alt="Group 15461" />
+                </div>
+                <p class="text-gray-600 text-mont text-mont text-base mt-4">
+                  Become a fundraiser and kick start your own Charity appeal
+                </p>
+              </div>
+            </div>
+            <div class="lg:w-2/3 w-full h-auto lg:px-4 mt-6 lg:mt-0">
+              <form onSubmit={handleSubmit}>
+                <input
+                  class="w-full h-auto text-black-50 text-xs text-mont font-medium bg-yellow border-2 border-lblack rounded-lg p-2"
+                  type="text"
+                  name="name"
+                  value={name}
+                  placeholder="Name & Surname *"
+                  onChange={e => setName(e.target.value)}
+                />
+                <input
+                  class="w-full h-auto text-black-50 text-xs text-mont font-medium bg-yellow border-2 border-lblack rounded-lg p-2 mt-4"
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Email *"
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <textarea
+                  class="w-full h-auto text-black-50 text-xs text-mont font-medium bg-yellow border-2 border-lblack rounded-lg p-2 mt-4"
+                  name="message"
+                  value={message}
+                  cols="30"
+                  rows="10"
+                  onChange={e => setMessage(e.target.value)}
+                  placeholder="Please provide as much detailed information as possible. Thank you *"
+                >
+                  Please provide as much detailed information as possible. Thank
+                  you *
+                </textarea>
+                <button class="lg:w-auto w-full text-xs text-mont text-black-50 font-bold rounded-lg bg-green text-center px-12 py-4 mt-4">
+                  SUBMIT MESSAGE
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeContact;
