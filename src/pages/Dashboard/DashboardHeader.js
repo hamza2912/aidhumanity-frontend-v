361b801c895d25bd7f6d79c473faf86f5436cf1c
muhammadfuaad <fuaad29@gmail.com>
2023-07-03 11:50:50 +0500

    hover added

--- src/pages/Dashboard/DashboardHeader.js
+++ src/pages/Dashboard/DashboardHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { SERVER_URL } from '../../services/config';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as User } from "../../images/user-circle-black.svg";
 
 const DashboardHeader = () => {
   const { user } = useSelector(state => state.session);
@@ -37,26 +38,24 @@ const DashboardHeader = () => {
             </button> */}
           </div>
           <div class="w-auto gap-4 flex justify-between items-center pl-16">
-            <button class="text-sm text-mont text-gray font-semibold hidden lg:block">
+            <button class="text-sm text-mont text-gray hover:text-dgray font-semibold hidden lg:block">
               En <i class="fa-solid fa-angle-down"></i>
             </button>
             <p class="text-sm text-mont text-gray font-semibold hidden lg:block">
               |
             </p>
             <a
-              className="text-sm text-mont text-black font-semibold flex items-center gap-2 cursor-pointer"
+              className="hover-button text-sm text-mont text-black font-semibold flex items-center gap-2 cursor-pointer"
               onClick={handleClick}
             >
-              <img
-                src={
-                  user?.avatar_link
-                    ? `${SERVER_URL + user.avatar_link}`
-                    : '/Icons/user-circle-white.svg'
-                }
-                alt="thumbnail"
-                className="w-6 h-full rounded-full"
-              />
-              <span>My Account</span>
+              {user?.avatar_link ?
+                <img
+                  src={SERVER_URL + user.avatar_link}
+                  alt="thumbnail"
+                  className="w-6 h-full rounded-full"
+                /> : < User className='icon w-5' />
+              }
+              <span className='hover:text-sblue'>My Account</span>
             </a>
           </div>
         </div>
