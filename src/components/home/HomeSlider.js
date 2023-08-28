79c4b8c8c615318be035a17e9f40dea1fddefd54
muhammadfuaad <fuaad29@gmail.com>
2023-08-28 23:10:02 +0500

    min-heights addded

--- src/components/home/HomeSlider.js
+++ src/components/home/HomeSlider.js
@@ -47,13 +47,13 @@ const HomeSlider = ({ appeals }) => {
         emulateTouch={true}
         preventMovementUntilSwipeScrollTolerance={true}
         infiniteLoop
-        autoPlay
+        // autoPlay
         showStatus={false}
       >
         {appeals.map(appeal => (
           <div
             className="w-full h-500px flex lg:flex-row flex-col py-8 group15343 container mx-auto 
-              lg:px-16 px-5 relative lg:my-16"
+              lg:px-16 px-5 relative lg:my-16 overflow-hidden lg:overflow-visible"
             key={('home-slider-item-', appeal.id)}
           >
             <img src="/Icons/blue_ring.svg" className='absolute -right-16 top-32 blue-ring'></img>
@@ -74,11 +74,11 @@ const HomeSlider = ({ appeals }) => {
                 </div>
               )}
               <h1 className="mt-2 lg:mt-4 text-left lg:text-[60px] leading-[3rem]
-                lg:leading-[4.5rem] text-[40px] text-mont font-bold text-white shadow-2">
+                lg:leading-[4.5rem] text-[40px] text-mont font-bold text-white shadow-2 min-h-36">
                 {appeal.title.split(' ')[0]} <br /> {appeal.title.split(' ').slice(1).join(' ')}
               </h1>
               <p className="mt-4 hidden lg:flex lg:w-3/5 2xl:w-1/2 text-white text-lg text-mont 
-                text-left opacity-70">
+                text-left opacity-70 min-h-[5.5rem]">
                 {textTruncate(appeal.story, 180)}
               </p>
               <div className="mt-6 lg:mt-10 flex gap-8">
