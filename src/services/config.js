37e7efe4bf66d59c6b774f2c38c17aefedc464c5
zeshan <zeshanahmedoff@gmail.com>
2023-10-09 14:39:16 +0500

    changes

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
-export const SERVER_API_URL = 'https://aidhumanity.org.uk/api';
-export const SERVER_URL = 'https://aidhumanity.org.uk';
-export const WEB_URL = 'https://aidhumanity.org.uk';
-// export const SERVER_API_URL = 'http://localhost:3001/api';
-// export const SERVER_URL = 'http://localhost:3001';
-// export const WEB_URL = 'http://localhost:3000';
+// export const SERVER_API_URL = 'https://aidhumanity.org.uk/api';
+// export const SERVER_URL = 'https://aidhumanity.org.uk';
+// export const WEB_URL = 'https://aidhumanity.org.uk';
+export const SERVER_API_URL = 'http://localhost:3001/api';
+export const SERVER_URL = 'http://localhost:3001';
+export const WEB_URL = 'http://localhost:3000';
 
 export const passwordRegex =
   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[\w!@#$%^&*()_+]{8,}$/;
